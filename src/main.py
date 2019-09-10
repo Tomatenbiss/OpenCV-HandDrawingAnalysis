33e26c8d6cf4e70e84cc29274c25fdbe617ab673
Tomatenbiss <s0545349@htw-berlin.de>
2019-09-10 11:52:15 +0200

    Dilate Image after HSV Filtering. Filter Contours by Area.

--- src/main.py
+++ src/main.py
@@ -10,6 +10,9 @@ if __name__ == "__main__":
         filename = '../data/im_temp_' + str(i + 1).zfill(2) +  '.jpg'
         im = cv2.imread(filename)
         im_reg = registrator.registerImage(im)
-        im_filt = ImageFilter.filterHSVRed(im_reg, 5, (40, 255), (110, 150))
-        im_contours = OpenCVHelper.drawContours(im_filt)
+        im_filt = ImageFilter.filterHSVRed(im_reg, 5, (40, 255), (110, 170))
+        im_filt = OpenCVHelper.dilateImage(im_filt)
+        contours, hierarchy = OpenCVHelper.getContours(im_filt)
+        contours = OpenCVHelper.filterContoursByArea(contours, 200)
+        im_contours = OpenCVHelper.drawContours(im_filt, contours=contours)
         OpenCVHelper.show(im_contours)
