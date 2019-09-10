7beabead4052a0790406f1da6c7c4852e37ab32c
Tomatenbiss <s0545349@htw-berlin.de>
2019-09-10 10:35:53 +0200

    Add drawContours method in OpenCVHelper.

--- src/OpenCVHelper.py
+++ src/OpenCVHelper.py
@@ -6,3 +6,13 @@ def show(im, im_size=(900, 900)):
     cv2.imshow('image', im)
     k = cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def drawContours(im, color=(0,255,0)):
+    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    _,th = cv2.threshold(imgray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    contours, hierarchy = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) > 0:
+        im = cv2.drawContours(im.copy(), contours, -1, color, 3)
+    return im
+    
+    
--- src/main.py
+++ src/main.py
@@ -7,4 +7,7 @@ if __name__ == "__main__":
     registrator = ImageRegistrator(cv2.imread('../data/im_ref.jpg'), aruco.Dictionary_get(aruco.DICT_4X4_50))
     for i in range(5):
         filename = '../data/im_temp_' + str(i + 1).zfill(2) +  '.jpg'
-        OpenCVHelper.show(registrator.registerImage(cv2.imread(filename)))
+        im = cv2.imread(filename)
+        im_reg = registrator.registerImage(im)
+        im_contours = OpenCVHelper.drawContours(im_reg)
+        OpenCVHelper.show(im_contours)
