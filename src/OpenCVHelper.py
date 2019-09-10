8c16b54fb1faed2c8755136f1ebb62b9805f6f3d
Tomatenbiss <s0545349@htw-berlin.de>
2019-09-10 14:08:07 +0200

    Add fitLineCV method.

--- src/OpenCVHelper.py
+++ src/OpenCVHelper.py
@@ -5,12 +5,27 @@ def dilateImage(im, kernelsize=5, iterations=1):
     kernel = np.ones((kernelsize,kernelsize),np.uint8)
     return cv2.dilate(im, kernel, iterations=1)
 
-def drawContours(im, color=(0,255,0), contours=None):
+def drawContours(im, color=(255,255,255), contours=None):
+    res = np.zeros(im.shape)
     if contours == None:
         contours, hierarchy = getContours(im)
     if len(contours) > 0:
-        im = cv2.drawContours(im.copy(), contours, -1, color, 3)
+        im = cv2.drawContours(res, contours, -1, color, cv2.FILLED)
     return im
+
+def fitLineCV(im):
+    empty = np.zeros(im.shape)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    rows,cols = gray.shape
+    pts = np.argwhere(gray == 255)
+    pts[:,[0,1]] = pts[:,[1,0]]
+    if len(pts) > 0:
+        [vx,vy,x,y] = cv2.fitLine(pts, cv2.DIST_L1, 0, 1, 0.01)
+        lefty = int((-x*vy/vx) + y)
+        righty = int(((cols-x)*vy/vx)+y)    
+        im = cv2.line(empty,(cols-1,righty),(0,lefty),(0,255,0),5)
+    return im
+    
 
 def getContours(im):
     imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
