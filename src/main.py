d41dc0b5d5efa9534baa778bbf1198df287cd305
Tomatenbiss <s0545349@htw-berlin.de>
2019-09-11 08:55:45 +0200

    Persist output files.

--- src/main.py
+++ src/main.py
@@ -7,10 +7,11 @@ import OpenCVHelper
 
 if __name__ == "__main__":
     registrator = ImageRegistrator(cv2.imread('../data/im_ref.jpg'), aruco.Dictionary_get(aruco.DICT_4X4_50))
-    for i in range(1):
+    for i in range(5):
         filename = '../data/im_temp_' + str(i + 1).zfill(2) +  '.jpg'
         im = cv2.imread(filename)
         im_reg = registrator.registerImage(im)
+        cv2.imwrite('../data/im_reg_' + str(i + 1).zfill(2) + '.jpg', im_reg)
         im_filt = ImageFilter.filterHSVRed(im_reg, 5, (40, 255), (110, 170))
         im_filt = OpenCVHelper.dilateImage(im_filt)
         contours, hierarchy = OpenCVHelper.getContours(im_filt)
@@ -19,7 +20,8 @@ if __name__ == "__main__":
         # OpenCVHelper.show(im_contours)
         imageTiler = ImageTiler(im_contours, 200, 200)
         tiles = imageTiler.getTiles()
-        for i in range(len(tiles)):
-            new_tile = tiles[i].astype('float32')
-            tiles[i] = OpenCVHelper.fitLinePCA(new_tile)
-        OpenCVHelper.show(imageTiler.reassembleImage(tiles))
+        for j in range(len(tiles)):
+            new_tile = tiles[j].astype('float32')
+            tiles[j] = OpenCVHelper.fitLinePCA(new_tile)
+        # OpenCVHelper.show(imageTiler.reassembleImage(tiles))
+        cv2.imwrite('../data/im_out_' + str(i + 1).zfill(2) + '.jpg', imageTiler.reassembleImage(tiles))
