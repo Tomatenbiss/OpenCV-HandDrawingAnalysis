55ebf3ffb3dc1698ce430c6de676474505fe6424
Tomatenbiss <s0545349@htw-berlin.de>
2019-09-12 09:37:03 +0200

    Add some documentation.

--- src/OpenCVHelper.py
+++ src/OpenCVHelper.py
@@ -3,10 +3,17 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 def dilateImage(im, kernelsize=5, iterations=1):
+    """ 
+    Applies OpenCVs dilate() method on the image.
+    """
     kernel = np.ones((kernelsize,kernelsize),np.uint8)
     return cv2.dilate(im, kernel, iterations=1)
 
 def drawContours(im, color=(255,255,255), contours=None):
+    """ 
+    Draws a set of contours on the image. If no contours are passed they 
+    are found using OpenCV's findContours() method.
+    """
     res = np.zeros(im.shape)
     if contours == None:
         contours, hierarchy = getContours(im)
@@ -15,6 +22,10 @@ def drawContours(im, color=(255,255,255), contours=None):
     return im
 
 def fitLineCV(im):
+    """
+    Fits a line through the white pixels of the image using OpenCV's fitline() 
+    method and returns a black image with the drawn line.
+    """
     empty = np.zeros(im.shape)
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     rows,cols = gray.shape
@@ -28,6 +39,10 @@ def fitLineCV(im):
     return im
     
 def fitLinePCA(im):
+    """
+    Fits a line through the white pixels of the image using sklearn's PCA class 
+    and returns a black image with the drawn line.
+    """
     empty = np.zeros(im.shape)
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     pts = np.argwhere(gray == 255)
@@ -45,17 +60,26 @@ def fitLinePCA(im):
     return im
 
 def getBinaryImage(im):
+    """
+    Returns the image converted to a binary image.
+    """
     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     _,res = cv2.threshold(im,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     return res
 
 def getContours(im):
+    """
+    Returns the contours found in the BGR image using OpenCV's findContours method.
+    """
     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     _,im = cv2.threshold(im,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     contours, hierarchy = cv2.findContours(im, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     return (contours, hierarchy)
 
 def filterContoursByArea(contours, threshold):
+    """
+    Returns all contours with an area greater than the threshold.
+    """
     res_contours = []
     for contour in contours:        
         if cv2.contourArea(contour) > threshold: 
@@ -63,6 +87,9 @@ def filterContoursByArea(contours, threshold):
     return res_contours
 
 def show(im, im_size=(900, 900)):
+    """
+    Displays the image in a window of size 900 x 900.
+    """
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('image', im_size[0], im_size[1])
     cv2.imshow('image', im)
