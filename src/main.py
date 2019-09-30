facef86e9356a4eaf13326502a1f57a305ed1845
Tomatenbiss <s0545349@htw-berlin.de>
2019-09-30 14:25:49 +0200

    Evaluate Drawing using ContourEvaluator.

--- src/main.py
+++ src/main.py
@@ -11,9 +11,10 @@ import OpenCVHelper
 if __name__ == "__main__":
     registrator = ImageRegistrator(cv2.imread('../data/im_ref.jpg'), aruco.Dictionary_get(aruco.DICT_4X4_50))
     # # for i in range(1):
-    filename = '../data/im_temp_07.jpg'
+    filename = '../data/im_temp_09.jpg'
     im = cv2.imread(filename)
     im_reg = registrator.registerImage(im)
+    # OpenCVHelper.show(im_reg)
     # cv2.imwrite('../data/im_reg_' + str(i + 1).zfill(2) + '.jpg', im_reg)
     im_filt = ImageFilter.filterHSVRed(im_reg, 10, (40, 255), (110, 170))
     im_filt = OpenCVHelper.dilateImage(im_filt)
@@ -31,10 +32,16 @@ if __name__ == "__main__":
     # # cv2.imwrite('../data/im_out_' + str(i + 1).zfill(2) + '.jpg', imageTiler.reassembleImage(tiles))
     
     im_mask = OpenCVHelper.getBinaryImage(cv2.imread(('../data/mask/teddy-10-outer-mask.jpg')))
+    im_mask = OpenCVHelper.getBinaryImage(cv2.imread(('../data/mask/AreaMask.jpg')))
     
     im_final = ImageFilter.filterByMask(im_contours, im_mask)
+    # OpenCVHelper.show(im_final)
     eval = ContourEvaluator.ContourEvaluator('../data/mask/distance/')
-    # eval.evaluateContours(im_final)
+    points = eval.evaluateContours(im_final)
+    total_contour_length = ImageAnalysis.getTotalContourLength(im_final)
+    print('Length: ' + str(total_contour_length))
+    print('Distance Error: ' + str(points))
+    print('Total: ' + str(int(total_contour_length / 5) + points))
     # print(ImageAnalysis.getTotalContourLength(im_final))
     
     # im_mask_inverted = OpenCVHelper.invert(im_mask)
